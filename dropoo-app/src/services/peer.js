5176f64ea6301327ab5e347db19e72e2cdf337ac
Kostas <kostasp@icloud.com>
2024-07-07 03:25:57 +0200

    fix build error

--- dropoo-app/src/services/peer.js
+++ dropoo-app/src/services/peer.js
@@ -115,14 +115,14 @@ class PeerService {
     this.socket.onclose = (event) => {
       console.log('Disconnected from signaling server:', event.reason);
     };
-    generatePeerId() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-      });
-    }
+    
   }
-
+  generatePeerId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
   handleServerMessage(data) {
     switch(data.type) {
       case 'registered':
