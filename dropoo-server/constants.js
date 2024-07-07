cc8335ddeeb11aca092e24bc5fba1e6391dd6a2b
Kostas <kostasp@icloud.com>
2024-07-07 13:46:54 +0200

    Update constants.js

--- dropoo-server/constants.js
+++ dropoo-server/constants.js
@@ -1,5 +1,7 @@
-export const greekNames = [
+ const greekNames = [
     "Zeus", "Hera", "Poseidon", "Demeter", "Athena", "Apollo", "Artemis", "Ares", "Aphrodite", "Hephaestus",
     "Hermes", "Dionysus", "Hades", "Persephone", "Hestia", "Eros", "Nike", "Iris", "Hecate", "Tyche",
     "Nemesis", "Hypnos", "Morpheus", "Nyx", "Thanatos", "Helios", "Selene", "Eos", "Aether", "Hemera"
   ];
+
+  module.exports = { greekNames };
