bb32b484f08fe14e82853379c5e782ca8d6883ac
Kostas <kostasp@icloud.com>
2024-12-17 13:37:29 +0100

    Revert "Fix Umami"
    This reverts commit 8f11622756d070e7f3df707ef950822344d80046.

--- dropoo-app/vue.config.js
+++ dropoo-app/vue.config.js
@@ -1,15 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack')
-const { umamiAnalyticsPlugin } = require('@vuepress/plugin-umami-analytics')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  plugins: [
-    umamiAnalyticsPlugin({
-      id: '1be950bc-ffb5-4890-a648-b6f50501edb6',
-      src: 'https://cloud.umami.is/script.js'
-    })
-  ],
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
