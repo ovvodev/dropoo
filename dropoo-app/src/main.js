97de3fdc57ea4ca7ee88af5254d59497e211b590
Kostas <kostasp@icloud.com>
2024-12-17 13:47:17 +0100

    test umami in main.js

--- dropoo-app/src/main.js
+++ dropoo-app/src/main.js
@@ -1,6 +1,16 @@
 import './process-polyfill'
 import { createApp } from 'vue'
+import { VueUmamiPlugin } from '@jaseeey/vue-umami-plugin';
+
 import App from './App.vue'
 import '../src/assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueUmamiPlugin, {
+    websiteID: '1be950bc-ffb5-4890-a648-b6f50501edb6',
+    scriptSrc: 'https://us.umami.is/script.js', // Optional
+    router
+});
+
+app.use(router).mount('#app');
